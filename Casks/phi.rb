c274a962f16e84bd3540b1af6bf1a866a7e4c18b
Kai Ren <kai@phi.cc>
2026-09-09 20:07:50 +0800

    Update Phi to 2.10.0,819 (#27)

--- Casks/phi.rb
+++ Casks/phi.rb
@@ -1,6 +1,6 @@
 cask "phi" do
-  version "2.9.0,807"
-  sha256 "65c8d7c3ddb4dc1a0a71fbfd1b4bc97f398c486b94a0d0c0fef46a1825b462bd"
+  version "2.10.0,819"
+  sha256 "8bc1d529d86f064968ddce7bad4a5b4dd388b5ca7426054211a5f897eabffb15"
 
   url "https://ota.phibrowser.com/mac-public/releases/Phi_#{version.csv.first}_#{version.csv.second}.zip"
   name "Phi"
